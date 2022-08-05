ae024a98707f9a46185a2ba35cba57a5b999fb59
YueunKim <yueunkim0605@gmail.com>
2022-08-05 23:02:49 +0900

    climate crisis

---
+++ main.js
@@ -0,0 +1,29 @@
+
+//Intro title
+// var textWrapper = document.querySelector('.txt');
+// var textWrapper_sub1 = document.querySelector('.txt_sub1');
+// var textWrapper_sub2 = document.querySelector('.txt_sub2');
+// var textWrapper_sub3 = document.querySelector('.txt_sub3');
+
+// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+// textWrapper_sub1.innerHTML = textWrapper_sub1.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+// textWrapper_sub2.innerHTML = textWrapper_sub2.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+// textWrapper_sub3.innerHTML = textWrapper_sub3.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+// anime.timeline()
+//   .add({
+//     targets: '.txt .letter',
+//     opacity: [0,1],
+//     easing: "easeInOutQuad",
+//     duration: 2250,
+//     delay: (el, i) => 100 * (i+1)
+//   });
+
+// anime.timeline()
+//   .add({
+//     targets: '.txt_sub1 .letter, .txt_sub2 .letter, .txt_sub3 .letter',
+//     opacity: [0,1],
+//     easing: "easeInOutQuad",
+//     duration: 2250,
+//     delay: (el, i) => 100 * (i+1)
+//   });
