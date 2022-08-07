860b559cb3b59c5ce9bb8d2dcbfcbd4fe20778dc
YueunKim <yueunkim0605@gmail.com>
2022-08-07 23:24:16 +0900

    climate

--- main.js
+++ main.js
@@ -1,29 +1,90 @@
+(() => {
+    //main2 제목
+    anime.timeline()
+    .add({
+        targets: '.main2_tit .line',
+        opacity: [0.5,1],
+        scaleX: [0, 1],
+        easing: "easeInOutExpo",
+        duration: 1000
+    }).add({
+        targets: '.main2_tit .line',
+        duration: 1000,
+        easing: "easeOutExpo",
+        translateY: (el, i) => (-0.625 + 0.625*2) + "em"
+    }).add({
+        targets: '.main2_tit .letters',
+        opacity: [0,1],
+        translateX: [0, 0],
+        easing: "easeOutExpo",
+        duration: 600,
+        offset: '-=900'
+    });
 
-//Intro title
-// var textWrapper = document.querySelector('.txt');
-// var textWrapper_sub1 = document.querySelector('.txt_sub1');
-// var textWrapper_sub2 = document.querySelector('.txt_sub2');
-// var textWrapper_sub3 = document.querySelector('.txt_sub3');
+    //main3 사진에 따라 텍스트 올라오기
+    const stepElems = document.querySelectorAll('.step');
+    const graphicElems = document.querySelectorAll('.graphic-item');
+    let currentItem = graphicElems[0]; // 현재 visible인 .graphic-item
+    let ioIndex;
 
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-// textWrapper_sub1.innerHTML = textWrapper_sub1.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-// textWrapper_sub2.innerHTML = textWrapper_sub2.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-// textWrapper_sub3.innerHTML = textWrapper_sub3.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    const io = new IntersectionObserver((entries, observer) => {
+        ioIndex = entries[0].target.dataset.index * 1;
+    });
 
-// anime.timeline()
-//   .add({
-//     targets: '.txt .letter',
-//     opacity: [0,1],
-//     easing: "easeInOutQuad",
-//     duration: 2250,
-//     delay: (el, i) => 100 * (i+1)
-//   });
+    for (let i = 0; i < stepElems.length; i++) {
+        io.observe(stepElems[i]);
+        stepElems[i].dataset.index = i;
+        graphicElems[i].dataset.index = i;
+    }
 
-// anime.timeline()
-//   .add({
-//     targets: '.txt_sub1 .letter, .txt_sub2 .letter, .txt_sub3 .letter',
-//     opacity: [0,1],
-//     easing: "easeInOutQuad",
-//     duration: 2250,
-//     delay: (el, i) => 100 * (i+1)
-//   });
+    function activate() { //visible 추가
+        currentItem.classList.add('visible');
+    }
+
+    function inactivate() { //visible 제거
+        currentItem.classList.remove('visible');
+    }
+
+    window.addEventListener('scroll', () => {
+        let step;
+        let boundingRect;
+
+        for (let i = ioIndex-1; i < ioIndex+2; i++){
+            step = stepElems[i];
+            if (!step) continue;
+            boundingRect = step.getBoundingClientRect();
+
+            if (boundingRect.top > window.innerHeight * 0.5 && 
+                boundingRect.top < window.innerHeight * 0.8) {
+                
+                if (currentItem) inactivate();
+
+                currentItem = graphicElems[step.dataset.index];
+                activate();
+            }
+        }
+    });
+
+    //main3 마지막 문구 타이핑
+    document.addEventListener('DOMContentLoaded',() => {
+        new TypeIt('.last #last_txt', {
+            speed: 200,
+            // waitUntilVisible: true,
+            // loop: true,
+            // breakLines: false
+        })
+        .type("당장 행동하지 않으면 다 같이 죽는다", {delay:1000})
+        .move(-10)
+        .delete(4, {delay:1000})
+        .move(+9)
+        .delete(2, {delay:1000})
+        .type("살 수 있")
+        .move(+1)
+        .go();  
+    })
+
+})();
+
+
+
+
